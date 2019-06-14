a0636a7b28be7c07de70ebdca5633bf14463e2fb
liusmallpig <chenhui.hu@carota.ai>
2019-06-14 17:22:01 +0800

    修改JNI接口

--- app/src/main/java/com/huxiaozhu/a7zip/MainActivity.java
+++ app/src/main/java/com/huxiaozhu/a7zip/MainActivity.java
@@ -2,23 +2,21 @@ package com.huxiaozhu.a7zip;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
-    static {
-        System.loadLibrary("native-lib");
-    }
 
+    @SuppressLint("DefaultLocale")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        TextView tv = findViewById(R.id.sample_text);
-        tv.setText(stringFromJNI());
+        TextView view = findViewById(R.id.textView);
+        view.setText(String.format("获取内容%d", ZipUtils.excuteCommand("")));
     }
 
-    public native String stringFromJNI();
 }
