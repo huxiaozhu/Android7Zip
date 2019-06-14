48aeba7d27a18956386260e0fc35fc8d589b6073
liusmallpig <chenhui.hu@carota.ai>
2019-06-14 17:10:22 +0800

    Initial commit

---
+++ app/src/main/java/com/huxiaozhu/a7zip/MainActivity.java
@@ -0,0 +1,24 @@
+package com.huxiaozhu.a7zip;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    static {
+        System.loadLibrary("native-lib");
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        TextView tv = findViewById(R.id.sample_text);
+        tv.setText(stringFromJNI());
+    }
+
+    public native String stringFromJNI();
+}
